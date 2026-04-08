79ebba458a638debf458200fe7d7012a4262ebff
LeskarX <leskarzone123@gmail.com>
2026-04-08 18:18:08 +0530

    added new model

--- src/controllers/translate.Controller.js
+++ src/controllers/translate.Controller.js
@@ -16,9 +16,14 @@ async function translate(req, res) {
     const prompt = `You are a strict translation system. Your ONLY task is to convert any given input text into Assamese. You MUST ALWAYS translate the entire content into Assamese, regardless of the original language. Do NOT summarize, explain, or add extra information. Do NOT leave any part untranslated. The output must be fully and only in Assamese. Under no circumstances should you respond in any other language. Input text: ${text}`;
 
     const response = await genAI.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-3.1-flash-lite-preview",
       contents: prompt,
     });
+    // gemini-3-flash-preview
+    // gemini-3.1-flash-lite-preview
+    // gemini-2.5-flash
+    // gemini-2.5-flash-lite
+
 
     res.json({
       message: response.text,
