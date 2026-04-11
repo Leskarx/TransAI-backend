88bb1f2127db138ed6eae63d7c89d5abe20831dc
LeskarX <leskarzone123@gmail.com>
2026-04-11 15:22:49 +0530

    pupetter issue

--- src/services/pdf.service.js
+++ src/services/pdf.service.js
@@ -18,7 +18,7 @@ function getImageSize(buffer) {
 export async function processPDF(buffer) {
   console.log(await puppeteer.executablePath());
 
-  // let browser;
+  let browser;
 
   try {
     console.log("📄 Extracting text...");
@@ -45,8 +45,8 @@ export async function processPDF(buffer) {
     const html = buildHtml(pageImages, textBlocks);
 
     // 🔹 Puppeteer
-    const browser = await puppeteer.launch({
-      executablePath: "/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.153/chrome-linux64/chrome",
+    browser = await puppeteer.launch({
+      executablePath: puppeteer.executablePath(), // ✅ FIX
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
